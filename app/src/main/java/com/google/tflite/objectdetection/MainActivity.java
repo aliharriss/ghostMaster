a198de99e7805680723dc28f8a93c2a96686c1e8
Ali Harris <aliharriss1995@gmail.com>
2020-02-25 00:28:28 +1300

    Added fragemnts as the main launcher, where the detect activity can be launched from. A train fragment needs to be developed that passes information to the detect activity to be able to run a workable ghost program

---
+++ app/src/main/java/com/google/tflite/objectdetection/MainActivity.java
@@ -0,0 +1,88 @@
+package com.google.tflite.objectdetection;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.tflite.objectdetection.tracking.CommunityFragment;
+import com.google.tflite.objectdetection.tracking.SettingsFragment;
+import com.google.tflite.objectdetection.tracking.StatsFragment;
+import com.google.tflite.objectdetection.tracking.TrainFragment;
+
+import org.tensorflow.lite.examples.detection.R;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new TrainFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_train);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_train:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TrainFragment()).commit();
+                break;
+            case R.id.nav_community:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CommunityFragment()).commit();
+                break;
+            case R.id.nav_stats:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new StatsFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_review:
+                Toast.makeText(this, "Review", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/google/tflite/objectdetection/tracking/CommunityFragment.java
@@ -0,0 +1,19 @@
+package com.google.tflite.objectdetection.tracking;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.tensorflow.lite.examples.detection.R;
+
+public class CommunityFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_community, container, false);
+    }
+}
